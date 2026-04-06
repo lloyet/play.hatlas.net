13ac0612e0a5bdb9af91103a6617710969398432
lloyet <contact@lloyet.dev>
2026-04-06 15:42:05 +0200

    first commit

---
+++ src/main/java/org/minecraft/atlas/Atlas.java
@@ -0,0 +1,16 @@
+package org.minecraft.atlas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Atlas extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
